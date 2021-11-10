f09e4bd7db13213886a71668ed6e7f4a10b30db1
Chukwuemeka Ashimole <eashimole@yahoo.com>
2021-11-10 23:19:47 +0100

    feat: add standardized api response

---
+++ app/Utils/ApiResponse.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Utils;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiResponse
+{
+
+    // Response 200 with data and message
+    static function ofData($data, ?string $message = null)
+    {
+        return response([
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ], Response::HTTP_OK);
+    }
+
+    // Response 200 with only message
+    static function ofMessage(string $message)
+    {
+        return response([
+            'success' => true,
+            'message' => $message
+        ], Response::HTTP_OK);
+    }
+
+    static function ofPaginatedData($data)
+    {
+        $metaData = [
+            'currentPage' => $data->currentPage(),
+            'hasMorePages' => $data->hasMorePages(),
+            'lastPage' => $data->lastPage(),
+            'nextPageUrl' => $data->nextPageUrl(),
+            'perPage' => $data->perPage(),
+            'previousPageUrl' => $data->previousPageUrl(),
+            'total' => $data->total(),
+            'url' => $data->url()
+        ];
+
+        $responseData = array_merge(['success' => true], $metaData, ['data' => $data]);
+        return response($responseData, Response::HTTP_OK);
+    }
+
+    static function ofUnauthorised(string $message = "Unauthorised")
+    {
+        return response([
+            'success' => false,
+            'message' => $message
+        ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    static function ofClientError(string $message = "Client Error", $errors = null)
+    {
+        return response([
+            'success' => false,
+            'message' => $message,
+            'errors' => $errors
+        ], Response::HTTP_BAD_REQUEST);
+    }
+
+    static function ofNotFound(string $message = "Not Found")
+    {
+        return response([
+            'success' => false,
+            'message' => $message
+        ], Response::HTTP_NOT_FOUND);
+    }
+
+    static function ofInternalServerError(string $message = "Internal Server Error")
+    {
+        return response([
+            'success' => false,
+            'message' => $message
+        ], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    static function ofForbidden(string $message = "Forbidden")
+    {
+        return response([
+            'success' => false,
+            'message' => $message
+        ], Response::HTTP_FORBIDDEN);
+    }
+
+}
