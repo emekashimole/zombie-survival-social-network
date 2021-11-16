65ef82fab908b74d7b3213b23915a02cc3322c6e
Chukwuemeka Ashimole <eashimole@yahoo.com>
2021-11-16 17:26:18 +0100

    feat: vue frontend

--- app/Utils/Constants.php
+++ app/Utils/Constants.php
@@ -5,7 +5,7 @@ namespace App\Utils;
 class Constants
 {
 
-    const PAGINATION_PER_PAGE = 20;
+    const PAGINATION_PER_PAGE = 5;
     const DEFAULT_DATE_SHORT_FORMAT = "M d, Y";
     const FLAG_COUNT_THRESHOLD = 3;
 
